94b0b1de906bb95aeb07c2f646012b2cf32dec6d
rwxdevjavu <aravindswaminathan2000@gmail.com>
2025-08-30 17:59:03 +0530

    localhost to vercel

--- frontend/src/app/component/portfolioTable.tsx
+++ frontend/src/app/component/portfolioTable.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useRef, useState } from "react";
 import MemoRow from "./memoRow";
 
-const apiUrl:string = process.env.NEXT_PUBLIC_WS_URL!;
+const wsUrl:string = process.env.NEXT_PUBLIC_API_URL!;
 
 export interface Stock {
   name: string;
@@ -42,7 +42,7 @@ export default function PortfolioTable() {
   const wsRef = useRef<WebSocket | null>(null);
 
   useEffect(() => {
-    fetch(apiUrl)
+    fetch(wsUrl+"/portfolio")
       .then((res) => res.json())
       .then((data) => setPortfolio(data))
       .catch((err) => console.error("Fetch error:", err));
@@ -52,7 +52,7 @@ export default function PortfolioTable() {
     let reconnectTimer: NodeJS.Timeout;
 
     const connect = () => {
-      const ws = new WebSocket("ws://localhost:8080");
+      const ws = new WebSocket(wsUrl);
       wsRef.current = ws;
 
       ws.onopen = () => {
